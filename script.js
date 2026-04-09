f9e0b9d544b83bb4f49b43a73062966511384b6e
VIDHYA SRI <vidhyasri0105@gmail.com>
2026-04-09 14:31:52 +0530

    round_2 js

---
+++ script.js
@@ -0,0 +1,33 @@
+
+var x = 10;
+let y = 20;
+const z = 30;
+
+x = 40; 
+y = 50; 
+z = 60; 
+
+
+a = 10
+b = 20
+
+function fn(hello) {
+    if (a == b) {
+        print.console("equal")
+    }
+    else {
+        print.console("not equal")
+    }
+}
+
+function vn(ans) {
+    if (a === b) {
+        print.console("equal")
+    }
+    else {
+        print.console("not equal")
+    }
+}
+
+const elem = document.getElementById('myDiv');
+elem.innerHTML = 'Hello!';
